c2c47ce175ab2e9509e68e89f182b7256037b760
xianxiango <513698824@qq.com>
2019-07-28 17:12:58 +0800

    commit

--- src/api/index.js
+++
@@ -1,8 +0,0 @@
-import qs from 'qs'
-import url from './url'
-import api from './config'
-
-// 请求接口
-export const fetchData = (name, params) => {
-  return api.post(url[name], qs.stringify(params)).then(data => data)
-}
--- src/api/url.js
+++
@@ -1,4 +0,0 @@
-export default {
-  'aaaa':'/gmn/banner/list'
-  
-}
